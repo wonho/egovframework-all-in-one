b76604be2709fd858e2c6644099063b1050c3952
wonho <helum@naver.com>
2016-07-20 17:28:00 +0900

    init egov

---
+++ src/main/java/egovframework/com/utl/sec/service/EgovCertInfoUtil.java
@@ -0,0 +1,75 @@
+package egovframework.com.utl.sec.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * GPKISecureWeb 인증서 로그인 서비스 유틸
+ * @author 공통컴포넌트개발팀 한성곤
+ * @since 2009.08.06
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ *
+ *   수정일      수정자           수정내용
+ *  -------    --------    ---------------------------
+ *   2009.08.06  한성곤          최초 생성
+ *
+ * </pre>
+ */
+public class EgovCertInfoUtil {
+    /**
+     * 서버인증서에 대한 Base64 정보를 얻는다.
+     *
+     * @return
+     * @throws ConfigurationException
+     * @throws GpkiApiException
+     */
+    public static String getBase64ServerCert() throws Exception {
+	/*
+	 * Configuration를 사용하기 위해서는 다음과 같은 시스템 변수 지정이 필요함
+	 *
+	 * -Dcom.dsjdf.config.file="/product/jeus/egovProps/gpkisecureweb/conf/dsjdf.properties"
+	 */
+//	Config dsjdf = new Configuration();
+//
+//	String certPath = dsjdf.get("GPKISecureWeb.CertFilePathName");
+//
+//	X509Certificate x509Cert = null;
+//	byte[] cert = null;
+//	String base64cert = null;
+//
+//	x509Cert = Disk.readCert(certPath);
+//	cert = x509Cert.getCert();
+//	Base64 base64 = new Base64();
+//	base64cert = base64.encode(cert);
+
+	return null;
+    }
+
+    /**
+     * 인증서에 대한 정보를 제공한다.
+     *
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    public static CertInfoVO getCertInfo(HttpServletRequest request) throws Exception {
+	CertInfoVO certInfo = new CertInfoVO();
+
+//	GPKIHttpServletRequest gpkirequest = null;
+//
+//	//System.out.println(request.getParameter("encryptedData"));
+//
+//	gpkirequest = new GPKIHttpServletRequest(request);
+//
+//	X509Certificate cert = gpkirequest.getSignerCert();
+//
+//	certInfo.setSubjectDn(cert.getSubjectDN());
+//	certInfo.setIssuerDn(cert.getIssuerDN());
+
+	return certInfo;
+    }
+}
